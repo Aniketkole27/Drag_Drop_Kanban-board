738e30e8d5821e31560250e1ff6ac31d1e28dbc8
Aniketkole27 <aniketkole07@gmail.com>
2024-09-22 11:14:00 +0530

    applied some styling a proper drops

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ boxes.forEach((box) =>{
 
 dragArea.forEach((drag) =>{
     drag.addEventListener("dragover",(event)=>{
+
         event.preventDefault();
+        const afterElement = getDragAfterElement(drag, event.clientY);
+        if (afterElement == null) {
+            drag.appendChild(dragItem);
+        } else {
+            drag.insertBefore(dragItem, afterElement);
+        }
     })
 
     drag.addEventListener("dragleave",()=>{
@@ -31,13 +38,32 @@ dragArea.forEach((drag) =>{
 
     drag.addEventListener("drop",(event)=>{
         drag.classList.remove("hover");
+        dragItem = null;
+        // if(dragItem){
+        //     drag.appendChild(dragItem)
+        //     dragItem = null;
+        // }
 
-        if(dragItem){
-            drag.appendChild(dragItem)
-            dragItem = null;
-        }
+        // if (dragItem) {
+        //     const container = drag.parentNode;
+        //     container.insertBefore(dragItem, drag);
+        // }
     })
 })
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.fbox:not(.hide)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.width;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
 
 
 // drag.addEventListener("dragover",(event) =>{
